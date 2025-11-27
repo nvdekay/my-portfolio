a6a87115c1521f6b0c032abd9e17dcf27fb6a433
nvdeekay <khanhnvd.work@gmail.com>
2025-11-27 10:30:20 +0700

    chore: fix version node

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ const App = () => {
                   <Projects />
                   <Certificates />
                   <Contact />
-                  <Chatbot />
+                  {/* <Chatbot /> */}
                 </>
               }
             />
